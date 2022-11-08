081df036fdb291554e839664ab1773e3a353a5f8
Jeongin Yoon <yoonjeongin@naver.com>
2022-11-09 05:24:23 +0900

    [level 0] Title: 숫자 찾기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.py
@@ -0,0 +1,9 @@
+def solution(num, k):
+    answer = 0
+    num = list(str(num))
+    print(num)
+    if str(k) in num:
+        answer = num.index(str(k)) + 1
+    else:
+        answer = -1
+    return answer
