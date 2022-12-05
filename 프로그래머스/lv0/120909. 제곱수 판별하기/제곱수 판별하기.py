874685187a8ab9fc2ac5cf90513b75b4185b9218
Jeongin Yoon <yoonjeongin@naver.com>
2022-12-05 12:50:32 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.py
@@ -0,0 +1,9 @@
+def solution(n):
+    answer = 0
+    for i in range(1, n):
+        if i * i == n:
+            answer = 1
+            break
+        else:
+            answer = 2
+    return answer
